74902d9dafe423195db8fffede7c4762b8d43215
Volkmar Rigo <Volkmar@rigo.bz>
2019-02-02 23:29:52 +0100

    Added Input BaseClass

---
+++ GoogleHashCode2018/Base/Input.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace GoogleHashCode2018.Base
+{
+    public abstract class Input<T>
+    {
+        protected string[] ParseParamLineString(string line)
+        {
+            return line.Split(' ');
+        }
+        protected int[] ParseParamLineInt(string line)
+        {
+            return ParseParamLineString(line).Select(int.Parse).ToArray();
+        }
+
+        public T Load(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath);
+            return Parse(lines);
+        }
+
+        public abstract T Parse(string[] input);
+    }
+}
