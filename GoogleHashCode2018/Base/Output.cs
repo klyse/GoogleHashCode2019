369717f34c838ac566810dd58619122e3a6e0756
Volkmar Rigo <Volkmar@rigo.bz>
2019-02-03 00:05:00 +0100

    Added Output and PizzaOutput

---
+++ GoogleHashCode2018/Base/Output.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace GoogleHashCode2018.Base
+{
+    public abstract class Output
+    {
+        protected string FixFileNameExt(string filePath)
+        {
+            if (string.Compare(Path.GetExtension(filePath), ".in", true) == 0)
+                return Path.ChangeExtension(filePath, ".out");
+
+            return filePath;
+        }
+
+        public void Save(String filePath)
+        {
+            filePath = FixFileNameExt(filePath);
+
+            File.WriteAllText(filePath, BuildOutputString());
+        }
+
+        protected abstract string BuildOutputString();
+    }
+}
---
+++ GoogleHashCode2018/Model/PizzaOutput.cs
@@ -0,0 +1,31 @@
+﻿using GoogleHashCode2018.Base;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace GoogleHashCode2018.Model
+{
+    public class PizzaOutput : Output
+    {
+        public class Slice
+        {
+            public int FromRow { get; set; }
+            public int ToRow { get; set; }
+            public int FromColumn { get; set; }
+            public int ToColumn { get; set; }
+        }
+
+        public List<Slice> Result = new List<Slice>();
+
+        protected override string BuildOutputString()
+        {
+            var output = new StringBuilder();
+            output.AppendLine($"{Result.Count}");
+            foreach (var item in Result)
+                output.AppendLine($"{item.FromRow} {item.ToRow} {item.FromColumn} {item.ToColumn}");
+
+            return  output.ToString();
+        }
+    }
+}
