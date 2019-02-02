565e67239012dbea638b8adddf7c33cec991c1ed
Volkmar Rigo <Volkmar@rigo.bz>
2019-02-03 00:06:05 +0100

    Added Solver and PizzaSolver1 Skeleton

---
+++ GoogleHashCode2018/Algorithms/PizzaSolver1.cs
@@ -0,0 +1,21 @@
+﻿using GoogleHashCode2018.Base;
+using GoogleHashCode2018.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleHashCode2018.Algorithms
+{
+    public class PizzaSolver1 : Solver<PizzaInput, PizzaOutput>
+    {
+        private PizzaOutput Result;
+
+        protected override PizzaOutput Solve(PizzaInput input)
+        {
+            Result = new PizzaOutput();
+
+
+            return Result;
+        }
+    }
+}
---
+++ GoogleHashCode2018/Base/Solver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleHashCode2018.Base
+{
+    public abstract class Solver<I, O> where O : Output
+    {
+        protected abstract O Solve(I input);
+
+        public O Execute(I input)
+        {
+            return Solve(input);
+        }
+    }
+}
