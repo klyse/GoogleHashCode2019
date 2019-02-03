70dac8671aff3ac175f17c0a0bd93a63a2451e39
Volkmar Rigo <Volkmar@rigo.bz>
2019-02-03 13:19:30 +0100

    Added Methods to Find rare CellType

--- GoogleHashCode2018/Algorithms/PizzaSolver1.cs
+++ GoogleHashCode2018/Algorithms/PizzaSolver1.cs
@@ -8,14 +8,34 @@ namespace GoogleHashCode2018.Algorithms
 {
     public class PizzaSolver1 : Solver<PizzaInput, PizzaOutput>
     {
-        private PizzaOutput Result;
-
-        protected override PizzaOutput Solve(PizzaInput input)
+        private (int tomatoCount, int mushroomCount) GetCellTypeCount()
         {
-            Result = new PizzaOutput();
+            int tomatoCount = 0;
+            int mushroomCount = 0;
+
+            for (int r = 0; r < Input.Rows; r++)
+                for (int c = 0; c < Input.Columns; c++)
+                    if (Input.Matrix[r, c] == PizzaInput.CellType.Tomato)
+                        tomatoCount++;
+                    else
+                        mushroomCount++;
+            return (tomatoCount, mushroomCount);
+        }
+
+        private PizzaInput.CellType FindCellTypeLowerCount()
+        {
+            var (tomatoCount, mushroomCount) = GetCellTypeCount();
+            if (tomatoCount < mushroomCount)
+                return PizzaInput.CellType.Tomato;
+            return PizzaInput.CellType.Mushroom;
+        }
+
+        protected override void Solve()
+        {
+            var rareCellType = FindCellTypeLowerCount();
 
 
-            return Result;
+
         }
     }
 }
