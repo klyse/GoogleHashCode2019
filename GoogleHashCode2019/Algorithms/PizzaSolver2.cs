f8b491a37bc8c33b462ab2a4d189dd9a6e094bb5
Unknown <Klaus.Pruenster@gmail.com>
2019-02-17 14:56:15 +0100

    updated test names

---
+++ GoogleHashCode2019/Algorithms/PizzaSolver2.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using GoogleHashCode2019.Base;
+using GoogleHashCode2019.Model;
+
+namespace GoogleHashCode2019.Algorithms
+{
+	public class PizzaSolver2 : Solver<PizzaInput, PizzaOutput>
+	{
+		protected override void Solve()
+		{
+		}
+	}
+}
