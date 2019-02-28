65198e3cae4cf805dd4b15523b567886145ef3b7
Volkmar Rigo <Volkmar@rigo.bz>
2019-02-28 20:41:11 +0100

    Moved Preprocessing in Method

--- GoogleHashCode2019/Algorithms/SlideShowSolver1.cs
+++ GoogleHashCode2019/Algorithms/SlideShowSolver1.cs
@@ -9,22 +9,31 @@ namespace GoogleHashCode2019.Algorithms
 	{
 		private TagMatrix Tags;
 
-		protected override void Solve()
+        private void CombineVertical()
+        {
+            var verticalPhotos = Input.Photos.Where(c => c.Orientation == Orientation.Vertical).ToList();
+            var combinedPhotos = new List<Photo>();
+
+
+            combinedPhotos.AddRange(Input.Photos.Where(c => c.Orientation == Orientation.Horizontal));
+            for (var i = 0; i < verticalPhotos.Count(); i += 2)
+            {
+                verticalPhotos[i].AddPhoto(verticalPhotos[i + 1]);
+                combinedPhotos.Add(verticalPhotos[i]);
+            }
+
+
+            Input.Photos = combinedPhotos;
+        }
+
+        protected override void Solve()
 		{
-			//Tags = new TagMatrix(Input);
+            //Tags = new TagMatrix(Input);
 
-			var verticalPhotos = Input.Photos.Where(c => c.Orientation == Orientation.Vertical).ToList();
-			var combinedPhotos = new List<Photo>();
+            CombineVertical();
 
 
-			combinedPhotos.AddRange(Input.Photos.Where(c => c.Orientation == Orientation.Horizontal));
-			for (var i = 0; i < verticalPhotos.Count(); i += 2)
-			{
-				verticalPhotos[i].AddPhoto(verticalPhotos[i + 1]);
-				combinedPhotos.Add(verticalPhotos[i]);
-			}
-
-			var orderedPhotos = combinedPhotos.OrderByDescending(c => c.Tags.Count);
+			var orderedPhotos = Input.Photos.OrderByDescending(c => c.Tags.Count);
 
 
 			foreach (var orderedPhoto in orderedPhotos)
