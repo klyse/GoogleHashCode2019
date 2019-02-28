bee5accad187fb4bc1b9a731be8c8d72a718512f
Volkmar Rigo <Volkmar@rigo.bz>
2019-02-28 20:26:41 +0100

    Auto stash before merge of "master" and "origin/master"

--- GoogleHashCode2019/Program.cs
+++ GoogleHashCode2019/Program.cs
@@ -10,7 +10,8 @@ namespace GoogleHashCode2019
 		{
 			var slideShowInput = new SlideShowInput().Load(Path.Combine(EnvironmentConstants.InputPath, "e_shiny_selfies.txt"));
 
-			var slideShowOutput = new SlideShowSolver1().Execute(slideShowInput);
+            
+            var slideShowOutput = new SlideShowSolver1().Execute(slideShowInput);
 
 			slideShowOutput.Save(Path.Combine(EnvironmentConstants.OutputPath, slideShowInput.FileName));
 		}
