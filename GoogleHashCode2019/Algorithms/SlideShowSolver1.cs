8c0ecade0ee3de89b5f9ffd7f1a6132d690cdcff
Volkmar Rigo <Volkmar@rigo.bz>
2019-02-28 19:46:44 +0100

    Merged

--- GoogleHashCode2019/Algorithms/SlideShowSolver1.cs
+++ GoogleHashCode2019/Algorithms/SlideShowSolver1.cs
@@ -5,9 +5,13 @@ namespace GoogleHashCode2019.Algorithms
 {
 	public class SlideShowSolver1 : Solver<SlideShowInput, SlideShowOutput>
 	{
+        private TagMatrix Tags;
+
 		protected override void Solve()
 		{
-			Output.Photos.Add(Photo.CreatePhoto("H 3 a b c", 0));
+            Tags = new TagMatrix(Input);
+
+            Output.Photos.Add(Photo.CreatePhoto("H 3 a b c", 0));
 			Output.Photos.Add(Photo.CreatePhoto("H 3 q b c", 0));
 			Output.Photos.Add(Photo.CreatePhoto("H 3 a b c", 0));
 		}
