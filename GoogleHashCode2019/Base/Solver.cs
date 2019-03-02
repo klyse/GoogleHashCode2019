cc529210eb34c979101487c0da1c8269c652854a
Volkmar Rigo <Volkmar@rigo.bz>
2019-03-02 14:40:18 +0100

    Added ExecuteAndSave Method to simplify testing

--- GoogleHashCode2019/Base/Solver.cs
+++ GoogleHashCode2019/Base/Solver.cs
@@ -1,6 +1,8 @@
-﻿namespace GoogleHashCode2019.Base
+﻿using System.IO;
+
+namespace GoogleHashCode2019.Base
 {
-	public abstract class Solver<TInput, TOutput> where TOutput : Output, new() where TInput : new()
+	public abstract class Solver<TInput, TOutput> where TOutput : Output, new() where TInput : Input, new()
     {
         protected TInput Input;
         protected TInput Work;
@@ -17,5 +19,21 @@
 
 			return Output;
 		}
-	}
+
+        public TOutput ExecuteAndSave(string inputPath, string outputPath, bool createSolverDir = false)
+        {
+            var input = new TInput();
+            input.Load(inputPath);
+
+            var output = Execute(input);
+            var fileName = outputPath;
+            if (createSolverDir)
+                fileName = Path.Combine(fileName, this.GetType().Name);
+            fileName = Path.Combine(fileName, Input.FileName);
+
+            output.Save(fileName);
+
+            return Output;
+        }
+    }
 }
