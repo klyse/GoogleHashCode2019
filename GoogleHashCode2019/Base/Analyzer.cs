5245f9d6840f31f1572a21bc697ded56ca47a47a
Volkmar Rigo <Volkmar@rigo.bz>
2019-03-02 18:14:18 +0100

    Added Analyzer Class

---
+++ GoogleHashCode2019/Base/Analyzer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleHashCode2019.Base
+{
+    public abstract class Analyzer<TInput> where TInput : Input
+    {
+        protected StringBuilder Result;
+        protected TInput Input;
+
+        public string Execute(TInput input)
+        {
+            Input = input;
+            Result = new StringBuilder();
+
+            Analyze();
+
+            return Result.ToString();
+        }
+
+        protected abstract void Analyze();
+    }
+}
