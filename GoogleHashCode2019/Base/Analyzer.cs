ee0ae64a9b1bbcf75270d860f6ea147e965d9909
Volkmar Rigo <Volkmar@rigo.bz>
2019-03-02 18:51:51 +0100

    Added ExecuteAndSave, CountItems and Dump for Dictionaries

--- GoogleHashCode2019/Base/Analyzer.cs
+++ GoogleHashCode2019/Base/Analyzer.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace GoogleHashCode2019.Base
 {
-    public abstract class Analyzer<TInput> where TInput : Input
+    public abstract class Analyzer<TInput> where TInput : Input, new()
     {
         protected StringBuilder Result;
         protected TInput Input;
 
         protected void Dump(string msg)
         {
+            Result.AppendLine(msg).AppendLine();
+        }
+
+        protected void Dump<K, V>(string msg, IEnumerable<KeyValuePair<K, V>> data)
+        {
+            var index = 0;
             Result.AppendLine(msg);
+            foreach (var item in data)
+                Result.AppendLine($"{++index}) {item.Key} = {item.Value}");
+
+            Result.AppendLine();
         }
 
         protected void CountItem<T>(Dictionary<T, int> countStore, T item)
@@ -19,6 +30,12 @@ namespace GoogleHashCode2019.Base
             if (!countStore.TryGetValue(item, out var count))
                 count = 0;
             countStore[item] = count + 1;
+        }
+
+        protected void CountItems<T>(Dictionary<T, int> countStore, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+                CountItem(countStore, item);
         }
 
         public string Execute(TInput input)
@@ -29,6 +46,27 @@ namespace GoogleHashCode2019.Base
             Analyze();
 
             return Result.ToString();
+        }
+
+        public string Execute(string inputPath)
+        {
+            var input = new TInput();
+            input.Load(inputPath);
+
+            return Execute(input);
+        }
+
+        public void ExecuteAndSave(string inputPath, string outputPath)
+        {
+            Execute(inputPath);
+            Save(outputPath);
+        }
+
+        public void Save(string outputPath)
+        {
+            var filePath = Path.Combine(outputPath, Path.ChangeExtension(Input.FileName, ".txt"));
+            Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+            File.WriteAllText(filePath, Result.ToString());
         }
 
         protected abstract void Analyze();
